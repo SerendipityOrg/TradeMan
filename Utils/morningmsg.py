56578de2ee813b61f1538c218683806b1e18b8c9
vanishree05 <serendipitytwo00@gmail.com>
2023-08-26 17:03:01 +0530

    morning message updated

--- Utils/morningmsg.py
+++ Utils/morningmsg.py
@@ -4,7 +4,12 @@ from datetime import date
 from babel.numbers import format_currency
 from pya3 import Aliceblue
 from kiteconnect import KiteConnect
+from telethon.sync import TelegramClient
 
+api_id = '22941664'
+api_hash = '2ee02d39b9a6dae9434689d46e0863ca'
+
+# Get the directory where the script is located
 script_dir = os.path.dirname(os.path.realpath(__file__))
 
 # Load user data from the JSON file
@@ -14,17 +19,14 @@ def load_userdata():
     with open(os.path.join(script_dir, "broker.json")) as f:
         return json.load(f)
 
-# Calculate invested value for AliceBlue user using the loop format
+# Calculate invested value for AliceBlue user
 
 
-# Calculate invested value for AliceBlue user using the loop format
 def aliceblue_invested_value(user_data):
-    # Initialization and other parts of your function
     alice = Aliceblue(user_data['username'], user_data['api_key'])
     session_id = alice.get_session_id()
     holdings = alice.get_holding_positions()
 
-    # Ensure 'HoldingVal' is a key in holdings and is of type list
     if not isinstance(holdings, dict) or 'HoldingVal' not in holdings or not isinstance(holdings['HoldingVal'], list):
         raise ValueError("Unexpected format for holdings data.")
 
@@ -33,10 +35,12 @@ def aliceblue_invested_value(user_data):
         average_price = float(stock['Price'])
         quantity = float(stock['HUqty'])
         invested_value += average_price * quantity
-    
+
     return invested_value
 
 # Calculate invested value for Zerodha user
+
+
 def zerodha_invested_value(broker_data, broker, user):
     user_details = broker_data[broker][user]
     kite = KiteConnect(api_key=user_details['api_key'])
@@ -46,6 +50,7 @@ def zerodha_invested_value(broker_data, broker, user):
 
 # Fetch invested value based on broker type
 
+
 def get_invested_value(broker_data, broker, user):
     user_details = broker_data[broker][user]
     if broker == "aliceblue":
@@ -53,31 +58,69 @@ def get_invested_value(broker_data, broker, user):
     elif broker == "zerodha":
         return zerodha_invested_value(broker_data, broker, user)
 
-# Generate morning report message for a user
-def report_msg(broker_data, broker, user):
-    user_data = broker_data[broker][user]
-    today = date.today()
-    formatted_date = today.strftime("%d %b %Y")
-    invested_value = get_invested_value(broker_data, broker, user)
-    cash_balance = user_data['expected_morning_balance'] - invested_value
-    current_capital = cash_balance + invested_value
+# Function to format currency in custom style
 
-    def custom_format(amount):
-        formatted = format_currency(amount, 'INR', locale='en_IN')
-        return formatted.replace('₹', 'Rs')
 
-    return (f"Morning Report for {user} on {formatted_date}:\n"
-            f"Yesterday's Capital: {custom_format(user_data['current_capital'])}\n"
-            f"Yesterday's PnL: {custom_format(user_data['yesterday_PnL'])}\n"
-            f"Cash Balance: {custom_format(cash_balance)}\n"
-            f"Stocks Invested: {custom_format(invested_value)}\n\n"
-            f"Current Capital: {custom_format(current_capital)}\n\n"
-            "Best regards,\nSerendipity Trading Firm")
+def custom_format(amount):
+    formatted = format_currency(amount, 'INR', locale='en_IN')
+    return formatted.replace('₹', 'Rs')
+
+# Generate a morning report message for a user
+
+
+def generate_message(user, formatted_date, user_data, cash_balance, invested_value, current_capital):
+    message = (
+        f"Morning Report for {user} on {formatted_date}:\n"
+        f"Yesterday's Capital: {custom_format(user_data['current_capital'])}\n"
+        f"Yesterday's PnL: {custom_format(user_data['yesterday_PnL'])}\n"
+        f"Cash Balance: {custom_format(cash_balance)}\n"
+        f"Stocks Invested: {custom_format(invested_value)}\n\n"
+        f"Current Capital: {custom_format(current_capital)}\n\n"
+        "Best regards,\nSerendipity Trading Firm"
+    )
+    return message
 
 
 # Main code execution
 userdata = load_userdata()
-for broker, data in userdata.items():
-    for user in data['accounts_to_trade']:
-        print(report_msg(userdata, broker, user))
 
+# Initialize an empty list for the accounts to trade
+user_list = []
+
+# Go through each broker and gather users
+for broker, broker_data in userdata.items():
+    if 'accounts_to_trade' in broker_data:
+        for account in broker_data['accounts_to_trade']:
+            user_list.append((broker, account))
+
+# Iterate through each user and generate and send report
+for broker, user in user_list:
+    user_data = userdata[broker][user]
+
+    # Calculate investment values
+    invested_value = get_invested_value(userdata, broker, user)
+    cash_balance = user_data['expected_morning_balance'] - invested_value
+    current_capital = cash_balance + invested_value
+
+    # Get current date in formatted style
+    today = date.today()
+    formatted_date = today.strftime("%d %b %Y")
+
+    # Generate the message
+    message = generate_message(
+        user, formatted_date, user_data, cash_balance, invested_value, current_capital)
+
+    # Print report for debugging purposes
+    print(message)
+
+    # # Load user-specific JSON data (assuming each user has a separate JSON)
+    # with open(os.path.join(script_dir, f"{user}.json")) as file:
+    #     specific_user_data = json.load(file)
+
+    # # Extract phone number
+    # phone_number = specific_user_data["mobile_number"]
+
+    # # Send the report to the user via Telegram
+    # # Ensure you have `api_id` and `api_hash` defined elsewhere in your code
+    # with TelegramClient('anon', api_id, api_hash) as client:
+    #     client.send_message(phone_number, message, parse_mode='md')
