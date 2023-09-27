7cd47a7a66147ab9c0dc5be7343aa9c8486a5535
vanishree05 <serendipitytwo00@gmail.com>
2023-09-27 16:59:52 +0530

    Daily Update
    omkar.xlsx DTD updated

--- Streamlit/dtdautomation.py
+++ Streamlit/dtdautomation.py
@@ -1,17 +1,97 @@
 import pandas as pd
 import os
 
+
 def has_required_columns(df):
-    """Check if all required columns are present in a DataFrame."""
-    required_columns = ['Date', 'PnL', 'Tax']
+    """Check if the DataFrame contains the required columns."""
+    required_columns = ['Date', 'Net PnL']
     return all(col in df.columns for col in required_columns)
 
-# Construct the path to the excel directory inside user profile
+
+def fetch_data_from_excel(file_name, sheet_mappings):
+    """Fetch data from Excel and return a dictionary of DataFrames."""
+    data_mappings = {}
+
+    # Iterating over each sheet mapping to read them into DataFrames
+    for internal_name, actual_sheet_name in sheet_mappings.items():
+        try:
+            temp_df = pd.read_excel(file_name, sheet_name=actual_sheet_name)
+
+            # Verify if the required columns exist in the sheet
+            if has_required_columns(temp_df):
+                data_mappings[internal_name] = temp_df
+            else:
+                print(
+                    f"Sheet '{actual_sheet_name}' in {user_file} does not have all required columns. Skipping...")
+
+        except ValueError:
+            print(
+                f"Sheet '{actual_sheet_name}' not found in {user_file}. Skipping...")
+
+    return data_mappings
+
+
+def create_dtd_dataframe(data_mappings):
+    """Creates and returns the DTD DataFrame."""
+    all_dates = pd.concat([df['Date']
+                          for df in data_mappings.values()]).unique()
+    all_dates_sorted = sorted(all_dates, key=pd.Timestamp)
+
+    rows = []
+
+    # List of transaction details we are interested in
+    default_details = ['MP Wizard', 'AmiPy', 'ZRM', 'Overnight Options',
+                       'Error Trade', 'Deposit', 'Withdrawal', 'Comission']
+    si_no = 1  # Initialize the serial number counter
+
+    # Adding the 'Opening Balance' entry once at the beginning
+    rows.append({
+        'SI NO': '',
+        'Date': '',
+        'Day': '',
+        'Trade ID': '',
+        'Details': 'Opening Balance',
+        'Amount': ''
+    })
+
+    # Iterating over each date to fetch transaction details
+    for date in all_dates_sorted:
+        for transaction_id in default_details:
+
+            # Initializing default amount string
+            amount_str = ''
+
+            # If the transaction_id exists in our data mappings (i.e., if we have a sheet/data for it)
+            if transaction_id in data_mappings:
+
+                # Fetch the DataFrame for that transaction_id
+                df = data_mappings[transaction_id]
+
+                # Get the 'Net PnL' value for the given date
+                amount = df[df['Date'] == date]['Net PnL'].sum()
+                amount_str = f'₹ {amount:,.2f}' if amount != 0 else ''
+
+            # Appending the transaction details to the rows list
+            rows.append({
+                'SI NO': si_no,
+                'Date': date.strftime('%d-%b-%y'),
+                'Day': date.strftime('%A'),
+                'Trade ID': '',
+                'Details': transaction_id,
+                'Amount': amount_str
+            })
+            si_no += 1
+
+    dtd_df = pd.DataFrame(rows)
+    return dtd_df
+
+
+# Directory and sheet mappings
 script_dir = os.path.dirname(os.path.realpath(__file__))
 user_profile = os.path.join(script_dir, '..', 'UserProfile')
 excel_dir = os.path.join(user_profile, 'excel')
 
-# Define the mapping of internal names to actual sheet names in the Excel
+# Sheet mappings to their corresponding internal names
 sheet_mappings = {
     'MP Wizard': 'MPWizard',
     'AmiPy': 'AmiPy',
@@ -19,136 +99,23 @@ sheet_mappings = {
     'Overnight Options': 'Overnight_options'
 }
 
-# Loop through all files in the excel directory and process them
+# Process each excel file in the directory
 for user_file in os.listdir(excel_dir):
-    if user_file.endswith('.xlsx') and user_file != "signal.xlsx":  # Skipping the file "signal.xlsx"
+    if user_file.endswith('.xlsx') and user_file != "signal.xlsx":
         file_name = os.path.join(excel_dir, user_file)
-        print(file_name)
 
-        # Initialize an empty dictionary to store the data
-        data_mappings = {}
+        # Fetching data from the excel file into a dictionary of DataFrames
+        data_mappings = fetch_data_from_excel(file_name, sheet_mappings)
 
-        # Loop through the sheet mappings and try to read each sheet
-        for internal_name, actual_sheet_name in sheet_mappings.items():
-            try:
-                temp_df = pd.read_excel(file_name, sheet_name=actual_sheet_name)
-                if not has_required_columns(temp_df):
-                    print(f"Sheet '{actual_sheet_name}' in {user_file} does not have all the required columns. Skipping...")
-                    continue
-                data_mappings[internal_name] = temp_df
-            except ValueError as e:
-                print(f"Sheet '{actual_sheet_name}' not found in {user_file}. Skipping...")
+        # Creating the DTD DataFrame
+        dtd_df = create_dtd_dataframe(data_mappings)
 
-
-        # Define the column headers for the new DTD DataFrame.
-        header = ['SI NO', 'Date', 'Day', 'Transaction Type', 'Opening Balance', 'MP Wizard', 'AmiPy', 'ZRM', 'Overnight Options', 'Error Trade','Gross PnL', 'Tax', 'Transaction Amount', 'Running Balance', 'Deposit','Withdrawal', 'Telegram Balance', 'Difference Amount', 'Remarks']
-
-        # Initialize an empty DataFrame with the defined headers.
-        dtd_df = pd.DataFrame(columns=header)
-
-        # Gather all unique dates from all sheets.
-        all_dates = pd.concat([df['Date'] for df in data_mappings.values()]).unique()
-
-        # Sort these dates.
-        sorted_dates = sorted(all_dates, key=pd.Timestamp)
-
-        # Assign the sorted dates to the 'Date' column of DTD DataFrame.
-        dtd_df['Date'] = sorted_dates
-
-        # Merge data from all sheets into the DTD DataFrame.
-        for column, df in data_mappings.items():
-            if 'PnL' in df.columns and 'Tax' in df.columns:
-                agg_df = df.groupby('Date').agg({'PnL': 'sum', 'Tax': 'sum'}).reset_index()
-                dtd_df = pd.merge(dtd_df, agg_df, on='Date', how='left', suffixes=('', f'_{column}'))
-                
-                # Rename the columns after merging.
-                if f'PnL_{column}' in dtd_df.columns:
-                    dtd_df[column] = dtd_df[f'PnL_{column}']
-                    dtd_df.drop(columns=[f'PnL_{column}'], inplace=True)
-                else:
-                    dtd_df[column] = dtd_df['PnL']
-                    dtd_df.drop(columns=['PnL'], inplace=True)
-
-        # Calculate total Tax and Gross PnL.
-        dtd_df['Tax'] = dtd_df[['Tax_' + name for name in data_mappings.keys()]].sum(axis=1)
-        dtd_df['Gross PnL'] = dtd_df[list(data_mappings.keys())].sum(axis=1)
-
-        # Remove individual tax columns after consolidating.
-        for col in dtd_df.columns:
-            if 'Tax_' in col:
-                dtd_df.drop(columns=[col], inplace=True)
-
-        # Add serial numbers.
-        dtd_df['SI NO'] = range(1, len(dtd_df) + 1)
-
-        # Convert 'Date' to datetime for sorting and operations.
-        dtd_df['Date'] = pd.to_datetime(dtd_df['Date'], errors='coerce')
-
-        # Extract 'Day' from the 'Date' column.
-        dtd_df['Day'] = dtd_df['Date'].dt.day_name()
-
-        # Set Transaction Type as "Trade" for all rows.
-        dtd_df['Transaction Type'] = "Trade"
-
-        # Convert 'Date' back to the desired string format.
-        dtd_df['Date'] = dtd_df['Date'].dt.strftime('%d-%b-%y')
-
-        # Format financial columns with currency notation.
-        currency_cols = ['Opening Balance', 'MP Wizard', 'AmiPy', 'ZRM', 'Overnight Options', 'Gross PnL', 'Tax', 'Transaction Amount', 'Running Balance', 'Deposit', 'Withdrawal', 'Telegram Balance', 'Difference Amount']
-        for col in currency_cols:
-            dtd_df[col] = dtd_df[col].apply(lambda x: f'₹ {x:,.2f}' if pd.notna(x) else '')
-
-        # Compute 'Running Balance' (Running Balance = Opening Balance - Transaction Amount)
-        dtd_df['Transaction Amount'] = dtd_df['Transaction Amount'].apply(lambda x: float(x.replace('₹', '').replace(',', '')) if x != '' else 0)
-
-        # Convert 'Opening Balance' to float
-        dtd_df['Opening Balance'] = dtd_df['Opening Balance'].apply(lambda x: float(x.replace('₹', '').replace(',', '')) if x != '' else 0)
-
-        # Calculate Running Balance
-        dtd_df['Running Balance'] = dtd_df['Opening Balance'] - dtd_df['Transaction Amount']
-
-        # Making today's 'Running Balance' the next day's 'Opening Balance'
-        dtd_df['Opening Balance'] = dtd_df['Running Balance'].shift(1, fill_value=0)
-
-        # Convert the financial values back to currency notation
-        dtd_df['Transaction Amount'] = dtd_df['Transaction Amount'].apply(lambda x: f'₹ {x:,.2f}')
-        dtd_df['Opening Balance'] = dtd_df['Opening Balance'].apply(lambda x: f'₹ {x:,.2f}' if pd.notna(x) else '')
-        dtd_df['Running Balance'] = dtd_df['Running Balance'].apply(lambda x: f'₹ {x:,.2f}' if pd.notna(x) else '')
-
-
-        # Compute 'Difference Amount' (difference between 'Running Balance' and 'Telegram Balance').
-        dtd_df['Difference Amount'] = dtd_df.apply(lambda row: float(row['Running Balance'].replace('₹', '').replace(',', '')) - float(row['Telegram Balance'].replace('₹', '').replace(',', '')) if row['Telegram Balance'] != '' else None, axis=1)
-        dtd_df['Difference Amount'] = dtd_df['Difference Amount'].apply(lambda x: f'₹ {x:,.2f}' if pd.notna(x) else '')
-
-        # Populate 'Remarks' column.
-        dtd_df['Remarks'] = dtd_df.apply(lambda row: 'Difference' if row['Difference Amount'] != '' else '', axis=1)
-
-        # Open the workbook and update/create the 'DTD' sheet
+        # Saving the DTD DataFrame back to the same excel file as a new sheet
         with pd.ExcelWriter(file_name, engine='openpyxl', mode='a') as writer:
-            # Load the entire workbook into memory
             book = writer.book
-
-            # Check if the 'DTD' sheet exists and remove it
             if 'DTD' in book.sheetnames:
-                existing_dtd_df = pd.read_excel(file_name, sheet_name='DTD')
-                
-                # Extract the last date from the existing 'DTD' sheet
-                last_date = pd.to_datetime(existing_dtd_df['Date'].iloc[-1], format='%d-%b-%y')
-                
-                # Filter the rows from dtd_df to only include rows with dates greater than last_date
-                dtd_df = dtd_df[pd.to_datetime(dtd_df['Date'], format='%d-%b-%y') > last_date]
-                
-                # Append new data to existing DTD sheet
-                combined_dtd_df = pd.concat([existing_dtd_df, dtd_df], ignore_index=True)
-                
-                # Remove the existing "DTD" sheet to be replaced
                 std = book['DTD']
                 book.remove(std)
-                
-            else:
-                combined_dtd_df = dtd_df
+            dtd_df.to_excel(writer, sheet_name='DTD', index=False)
 
-            # Save only the 'DTD' sheet, keeping other sheets untouched
-            combined_dtd_df.to_excel(writer, sheet_name='DTD', index=False)
-
-        print("DTD sheet has been updated!")
+        print(f"{user_file} has been updated with a DTD sheet!")
