6b9197ba8c5c058614c2a8a72f6ce38a0f1d08e8
vanishree05 <serendipitytwo00@gmail.com>
2023-10-05 17:08:05 +0530

    dailyupdate
    omkar.xlsx updated
    DTD updated

--- Streamlit/dtdautomation.py
+++ Streamlit/dtdautomation.py
@@ -1,41 +1,22 @@
 import pandas as pd
 import os
+from babel.numbers import format_currency
 
-# Function to format numbers according to the Indian numbering system.
-
-
-def format_indian(number):
-    s = '%.2f' % number
-    integer_part, fractional_part = s.split('.')
-    integer_part = int(integer_part)
-
-    # Create the formatted integer part
-    thousands = integer_part % 1000
-    integer_part //= 1000
-
-    lakhs = integer_part % 100
-    integer_part //= 100
-
-    crores = integer_part
-
-    if crores:
-        result = '{}, {:02d}, {:03d}'.format(crores, lakhs, thousands)
-    elif lakhs:
-        result = '{:02d}, {:03d}'.format(lakhs, thousands)
-    else:
-        result = '{:03d}'.format(thousands)
-
-    return result + '.' + fractional_part
+# Function to format the 'Running Balance' column
 
 
-# Check if the DataFrame contains the required columns.
+def format_running_balance_column(df):
+    df['Running Balance'] = df['Running Balance'].apply(custom_format)
+    return df
+
+# Function to check if DataFrame has required columns
 
 
 def has_required_columns(df):
     required_columns = ['Date', 'Net PnL', 'Trade ID']
     return all(col in df.columns for col in required_columns)
 
-# Fetch data from Excel and return a dictionary of DataFrames.
+# Function to fetch data from Excel and return a dictionary of DataFrames
 
 
 def fetch_data_from_excel(file_name, sheet_mappings):
@@ -54,10 +35,24 @@ def fetch_data_from_excel(file_name, sheet_mappings):
                 f"Sheet '{actual_sheet_name}' not found in {file_name}. Skipping...")
     return data_mappings
 
-# Create and return the DTD DataFrame with correct running balance calculation and formatted columns.
+# Custom format function for currency
 
 
-def create_dtd_dataframe_updated_v3(data_mappings, opening_balance):
+def custom_format(amount):
+    if isinstance(amount, (int, float)):
+        formatted = format_currency(amount, 'INR', locale='en_IN')
+        return formatted.replace('₹', '₹ ')
+    elif isinstance(amount, str):
+        # Handle the case where 'amount' is already a formatted string
+        return amount
+    else:
+        # Return the value as is (not formatted)
+        return amount
+
+# Function to create and return the DTD DataFrame with correct running balance calculation and formatted columns
+
+
+def create_dtd_dataframe_updated_v10(data_mappings, opening_balance):
     if not data_mappings:
         print("No valid DataFrames found!")
         return pd.DataFrame(), 0
@@ -80,8 +75,9 @@ def create_dtd_dataframe_updated_v3(data_mappings, opening_balance):
         'Day': 'Friday',
         'Trade ID': '',
         'Details': 'Opening Balance',
-        'Amount': '₹ ' + format_indian(opening_balance),
-        'Running Balance': '₹ ' + format_indian(running_balance)
+        'Amount': custom_format(running_balance),  # Format the opening balance
+        # Format the running balance
+        'Running Balance': custom_format(running_balance)
     })
     sl_no += 1
 
@@ -110,8 +106,29 @@ def create_dtd_dataframe_updated_v3(data_mappings, opening_balance):
                             'Day': day_str,
                             'Trade ID': trade_id,
                             'Details': transaction_id,
-                            'Amount': '₹ ' + format_indian(amount),
-                            'Running Balance': '₹ ' + format_indian(running_balance)
+                            # Format the amount
+                            'Amount': custom_format(amount),
+                            # Format the running balance
+                            'Running Balance': custom_format(running_balance)
+                        })
+                        sl_no += 1
+                elif transaction_id == "ZRM":
+                    aggregated_pnl = sub_df['Net PnL'].sum()
+                    if aggregated_pnl != 0.00:
+                        aggregated_trade_ids = ' '.join(
+                            sub_df['Trade ID'].dropna())
+                        amount = aggregated_pnl
+                        running_balance += amount  # Update running balance BEFORE appending
+                        rows.append({
+                            'Sl NO': sl_no,
+                            'Date': date_str,
+                            'Day': day_str,
+                            'Trade ID': aggregated_trade_ids,
+                            'Details': transaction_id,
+                            # Format the amount
+                            'Amount': custom_format(amount),
+                            # Format the running balance
+                            'Running Balance': custom_format(running_balance)
                         })
                         sl_no += 1
                 else:
@@ -127,15 +144,17 @@ def create_dtd_dataframe_updated_v3(data_mappings, opening_balance):
                             'Day': day_str,
                             'Trade ID': aggregated_trade_ids,
                             'Details': transaction_id,
-                            'Amount': '₹ ' + format_indian(amount),
-                            'Running Balance': '₹ ' + format_indian(running_balance)
+                            # Format the amount
+                            'Amount': custom_format(amount),
+                            # Format the running balance
+                            'Running Balance': custom_format(running_balance)
                         })
                         sl_no += 1
 
     dtd_df = pd.DataFrame(rows)
     return dtd_df, running_balance
 
-# Retrieve existing 'Opening Balance' from the DTD sheet.
+# Function to retrieve existing 'Opening Balance' from the DTD sheet
 
 
 def get_existing_opening_balance(file_name):
@@ -149,7 +168,7 @@ def get_existing_opening_balance(file_name):
                 return float(str(running_balance_str).replace('₹', '').replace(',', '').strip())
     return None
 
-# Append new data to the existing DTD sheet or create a new one.
+# Function to append new data to the existing DTD sheet or create a new one
 
 
 def check_and_update_dtd_sheet(file_name, new_dtd_df):
@@ -163,11 +182,12 @@ def check_and_update_dtd_sheet(file_name, new_dtd_df):
     with pd.ExcelWriter(file_name, engine='openpyxl', mode='a') as writer:
         if existing_opening_balance is not None:
             new_dtd_df.loc[new_dtd_df['Details'] == 'Opening Balance',
-                           'Running Balance'] = '₹ ' + format_indian(existing_opening_balance)
+                           'Running Balance'] = custom_format(existing_opening_balance)  # Format the existing opening balance
 
         if 'DTD' in writer.book.sheetnames:
             existing_dtd = pd.read_excel(file_name, sheet_name='DTD')
-
+            existing_dtd = format_running_balance_column(
+                existing_dtd)  # Format the existing data
             if 'Date' in existing_dtd.columns and 'Date' in new_dtd_df.columns:
                 last_existing_date = pd.to_datetime(
                     existing_dtd['Date'].iloc[-1])
@@ -188,11 +208,31 @@ def check_and_update_dtd_sheet(file_name, new_dtd_df):
         updated_dtd_df.to_excel(writer, sheet_name='DTD', index=False)
 
 
+# Function to read opening balances from useropeningbalance.txt and return as a dictionary
+def read_opening_balances(file_path):
+    opening_balances = {}
+    try:
+        with open(file_path, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                parts = line.strip().split(':')
+                if len(parts) == 2:
+                    user = parts[0].strip()
+                    balance_str = parts[1].strip().replace(
+                        '₹', '').replace(',', '').strip()
+                    opening_balances[user] = float(balance_str)
+    except FileNotFoundError:
+        print("useropeningbalance.txt not found.")
+    return opening_balances
+
+
 # Main execution
 if __name__ == "__main__":
     script_dir = os.path.dirname(os.path.realpath(__file__))
     user_profile = os.path.join(script_dir, '..', 'UserProfile')
     excel_dir = os.path.join(user_profile, 'excel')
+    opening_balances = read_opening_balances(
+        os.path.join(script_dir, 'useropeningbalance.txt'))
 
     sheet_mappings = {
         'MP Wizard': 'MPWizard',
@@ -201,16 +241,16 @@ if __name__ == "__main__":
         'Overnight Options': 'Overnight_options'
     }
 
-    for user_file in os.listdir(excel_dir):
-        if user_file.endswith('.xlsx') and user_file != "signal.xlsx":
-            file_name = os.path.join(excel_dir, user_file)
-            data_mappings = fetch_data_from_excel(file_name, sheet_mappings)
-
-            # Get the existing opening balance
-            existing_opening_balance = get_existing_opening_balance(file_name)
-            if existing_opening_balance is None:
-                existing_opening_balance = 0.0  # Default value if no opening balance exists
-
-            dtd_df, _ = create_dtd_dataframe_updated_v3(
-                data_mappings, existing_opening_balance)
-            check_and_update_dtd_sheet(file_name, dtd_df)
+    for root, dirs, files in os.walk(excel_dir):
+        for file in files:
+            if file.endswith(".xlsx"):
+                file_name = os.path.join(root, file)
+                # Extract user name from file name
+                user_name = os.path.splitext(file)[0]
+                # Get the opening balance for the user
+                opening_balance = opening_balances.get(user_name, 0.0)
+                data_mappings = fetch_data_from_excel(
+                    file_name, sheet_mappings)
+                dtd_df, _ = create_dtd_dataframe_updated_v10(
+                    data_mappings, opening_balance)
+                check_and_update_dtd_sheet(file_name, dtd_df)
